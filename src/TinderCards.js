787d852cd9f65007196b0344dba7bafbd00a48b6
Arpit Adlakha <arpit.adlakha@gojek.com>
2021-06-13 22:51:50 +0530

    Remove useState and useEffect hooks

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import TinderCard  from "react-tinder-card";
 // import database from "./firebase";
 import "./TinderCards.css"
